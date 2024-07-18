93af132e74ad90efcccc74a4759ba80a5c55e942
hatoo <hato2000@gmail.com>
2024-07-18 16:58:25 +0900

    codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use ecow::EcoString;
 
 use crate::tacky;
@@ -75,6 +77,10 @@ fn gen_function(function: &tacky::Function) -> Function {
         }
     }
 
+    let stack_size = pseudo_to_stack(&mut body);
+    body.insert(0, Instruction::AllocateStack(stack_size));
+    body = avoid_mov_mem_mem(body);
+
     Function {
         name: function.name.clone(),
         body,
@@ -86,4 +92,57 @@ fn val_to_operand(val: &tacky::Val) -> Operand {
         tacky::Val::Constant(imm) => Operand::Imm(*imm),
         tacky::Val::Var(var) => Operand::Pseudo(var.clone()),
     }
+}
+
+fn pseudo_to_stack(insts: &mut [Instruction]) -> usize {
+    let mut known_vars = HashMap::new();
+
+    let mut remove_pseudo = |operand: &mut Operand| match operand {
+        Operand::Pseudo(var) => {
+            let rsp = (known_vars.len() as i32 + 1) * -4;
+            let offset = known_vars.entry(var.clone()).or_insert(rsp);
+            *operand = Operand::Stack(*offset);
+        }
+        _ => {}
+    };
+
+    for inst in insts {
+        match inst {
+            Instruction::Mov { src, dst } => {
+                remove_pseudo(src);
+                remove_pseudo(dst);
+            }
+            Instruction::Unary { src, .. } => {
+                remove_pseudo(src);
+            }
+            Instruction::AllocateStack(_) => {}
+            Instruction::Ret => {}
+        }
+    }
+
+    known_vars.len() * 4
+}
+
+fn avoid_mov_mem_mem(insts: Vec<Instruction>) -> Vec<Instruction> {
+    let mut new_insts = Vec::new();
+
+    for inst in insts {
+        match inst {
+            Instruction::Mov { src, dst }
+            // ????
+                if matches!((&src, &dst), (Operand::Stack(_), Operand::Stack(_))) =>
+            {
+                new_insts.push(Instruction::Mov {
+                    src,
+                    dst: Operand::Reg(Register::R10),
+                });
+                new_insts.push(Instruction::Mov {
+                    src: Operand::Reg(Register::R10),
+                    dst,
+                });
+            }
+            _ => new_insts.push(inst),
+        }
+    }
+    new_insts
 }
