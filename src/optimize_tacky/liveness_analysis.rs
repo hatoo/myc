797b1addc40809aa1e56b5e3884e9365eb3328a4
hatoo <hato2000@gmail.com>
2024-09-05 13:37:14 +0900

    wip

--- src/optimize_tacky/liveness_analysis.rs
+++ src/optimize_tacky/liveness_analysis.rs
@@ -206,7 +206,11 @@ impl Annotation {
                 let live_variables = &self.instruction_annotation[&node.id][i];
                 if let Some(dst) = dst_field(inst) {
                     if !live_variables.contains(dst) {
-                        *inst = Instruction::Nop;
+                        if let Instruction::FunCall { dst, .. } = inst {
+                            *dst = None;
+                        } else {
+                            *inst = Instruction::Nop;
+                        }
                     }
                 }
             }
@@ -223,6 +227,7 @@ fn dst_field(inst: &Instruction) -> Option<&EcoString> {
         | Instruction::Cast { dst, .. }
         | Instruction::Copy { dst, .. }
         | Instruction::GetAddress { dst, .. }
+        | Instruction::FunCall { dst: Some(dst), .. }
         | Instruction::CopyFromOffset { dst, .. } => Some(dst.var()),
         Instruction::CopyToOffset { dst, .. } => Some(dst),
         _ => None,
